b5365d4ea5bd639971f0c14f01cb6bff99745f9e
ravindra1234567 <ravindrakushwahanwg@gmail.com>
2020-01-17 18:44:16 +0530

    indented

--- application/models/Alumni_Model.php
+++ application/models/Alumni_Model.php
@@ -3,12 +3,12 @@ class Alumni_model extends CI_Model {
 	function __construct(){
 		parent::__construct();
 	}
-
 	public function authenticateAlumni($data) {
 		if ($data == null){
 			return array("id"=>0);
 		}
-		else {
+		else
+		{
 			$result = $this->db->get_where('admin', $data); 
 			if ($result->num_rows()) {
 				return array("id"=>$result->result_array()[0]["id"], "name"=>$result->result_array()[0]["name"], "college_id" => $result->result_array()[0]["college_id"]);
