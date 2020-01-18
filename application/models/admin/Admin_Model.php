30d31a14cd168583bb133ea774682da4684f19c3
sumit684 <singh.sumit684@gmail.com>
2020-01-18 13:51:11 +0530

    Update Admin_Model.php

--- application/models/admin/Admin_Model.php
+++ application/models/admin/Admin_Model.php
@@ -5,17 +5,15 @@ class Admin_model extends CI_Model {
 	}
 	
 	public function getregisteredAlumni(){
+		$this->db->order_by("created", "asc");
+		return $this->db->get_where('alumni',array('status'=>'1'))->result();
 		
-		$this->db->get_where('alumni',array('status'=>'1'))->result_array();
-		$result = $this->db->order_by("created", "asc");
-		return $result;
-
 	}
 
 	public function getnewstudents(){
-		$this->db->get_where('alumni',array('status'=>'0'))->result_array();
-		$result = $this->db->order_by("created", "asc");
-		return $result;
+		// $this->db->get_where('alumni',array('status'=>'0'));
+		// $result = $this->db->order_by("created", "asc")->result();
+		// return $result;
 	}
 }
 ?>
