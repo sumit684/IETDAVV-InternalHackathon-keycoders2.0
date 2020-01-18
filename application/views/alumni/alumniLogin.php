87b176d68e6e11a19e1d66470496eed56e0c6237
Pratik Mehta <43946536+Pratikmehta1729@users.noreply.github.com>
2020-01-18 16:09:53 +0530

    login

--- application/views/alumni/alumniLogin.php
+++ application/views/alumni/alumniLogin.php
@@ -23,7 +23,7 @@
 		<div class="d-flex justify-content-center h-100">
 			<div class="card">
 				<div class="card-header">
-					<h3>Sign In</h3>
+					<h3>Alumni Sign In</h3>
 					<div class="d-flex justify-content-end social_icon">
 						<span><i class="fab fa-facebook-square"></i></span>
 						<span><i class="fab fa-google-plus-square"></i></span>
